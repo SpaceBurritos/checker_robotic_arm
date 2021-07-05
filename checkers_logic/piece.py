22a531f83958f76f1941dbddc2bc50bcf0676b8b
SpaceBurritos <59981149+SpaceBurritos@users.noreply.github.com>
2021-07-06 07:12:41 +0800

    Add files via upload

--- checkers_logic/piece.py
+++ checkers_logic/piece.py
@@ -6,15 +6,17 @@ RIGHT = 1
 
 class Piece:
 
-    def __init__(self, pos, color, is_king=False):
+    def __init__(self, pos, color):
         self.y = pos[0]
         self.x = pos[1]
         self.color = color
-        self.king = is_king
-        self.forward = 1 if color == "black" else -1
+        self.forward = 1 if self.y < 4 else -1
+        self.king = self.make_king()
+        self.is_jumping = False
         self.valid_moves = self.poss_moves()
         self.next_moves = []
         self.can_jump = False
+
         self.del_pieces = []
         pass
 
@@ -31,7 +33,7 @@ class Piece:
             moves.append(left)
         if 7 >= right[0] >= 0 and 7 >= right[1]:
             moves.append(right)
-        if self.king:
+        if self.king or self.is_jumping:
             left = [self.y - self.forward, self.x + LEFT]
             right = [self.y - self.forward, self.x + RIGHT]
             if 7 >= left[0] >= 0 and left[1] >= 0:
@@ -51,8 +53,10 @@ class Piece:
         king_y = 7 if self.forward == 1 else 0
         if self.y == king_y and not self.king:
             self.king = True
-        elif self.y == king_y:
-            print("Already a king")
+            self.valid_moves = self.poss_moves()
+            return True
+
+        return False
 
     def set_can_jump(self, val):
         self.can_jump = val
@@ -67,7 +71,18 @@ class Piece:
             print("There is no skipped position")
 
     def __eq__(self, other):
-        return [other.y, other.x] == [self.y, self.x]
+
+        if type(other) is list:
+            return other == [self.y, self.x]
+        else:
+            return [other.y, other.x] == [self.y, self.x]
 
     def __str__(self):
         return "[" + str(self.y) + ", " + str(self.x) + "]"
+        
+        
+if __name__ == "__main__":
+    ls = np.array([2,3])
+    p1 = Piece([3,3], "black")
+    p = Piece([2,3], "black")
+    print(ls == p)
